d48eb308fed04643490e7f2e4a01d3b0b655a54f
Joren <babamyloves2002@gmail.com>
2023-07-31 09:54:40 +0800

    ambatoUPDATE

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  publicPath: '/weather-forecast-app/',
+  publicPath: '/weather-forecast/',
 
   pluginOptions: {
     vuetify: {
